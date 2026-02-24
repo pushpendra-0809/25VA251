9dbf18ea7afb4002c26adef3827157504f005048
Pushpendra Kumar Verma <vermapushpendra0809@gmail.com>
2026-02-24 16:27:20 +0530

    Introduction to Variables in JSS

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Pushpendra");
+{
+    let a = "hello";
+    console.log(a);
+    // console.log(typeof a);
+    var b = 10;
+    console.log(b);
+    b = 90.21;
+    console.log(b);
+    console.log(typeof b);
+}
+// console.log(a); a ka scope nhi hai yahan
+console.log(b);
